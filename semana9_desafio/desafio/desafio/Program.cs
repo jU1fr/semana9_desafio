d48a1fbad62cd337072f22b8e4d34d60d996bab7
Juan Carlos Manuel Lechuga Ortiz21 <134109649+jU1fr@users.noreply.github.com>
2024-04-10 16:05:47 -0600

    Add files via upload

---
+++ semana9_desafio/desafio/desafio/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class EmailValidator
+{
+    public static void Main(string[] args)
+    {
+        string text = "Este es mi correo electrónico: juan_perez.23@gmail.com. Por favor, envíeme un correo con sus comentarios.";
+        string[] emails = ExtractEmails(text);
+        foreach (string email in emails)
+        {
+            Console.WriteLine(email);
+        }
+        Console.WriteLine("Envíame un correo con sus comentarios");
+    }
+
+    public static string[] ExtractEmails(string text)
+    {
+        string pattern = @"\b[\w\.-]+@[\w\.-]+\.\w+\b";
+        MatchCollection matches = Regex.Matches(text, pattern);
+        string[] validEmails = new string[matches.Count];
+        int validCount = 0;
+        foreach (Match match in matches)
+        {
+            string email = match.Value;
+            if (IsValidEmail(email))
+            {
+                validEmails[validCount] = email;
+                validCount++;
+            }
+        }
+        Array.Resize(ref validEmails, validCount);
+        return validEmails;
+    }
+
+    public static bool IsValidEmail(string email)
+    {
+        string pattern = @"^[\w\.-]+@[\w\.-]+\.\w+$";
+        return Regex.IsMatch(email, pattern);
+    }
+}
